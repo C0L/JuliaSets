4a6afbb49904e310fde5b182f593f4d401e3b96e
C0L <colindrewes@gmail.com>
2021-05-19 12:51:36 -0700

    Squash

--- mpi_c/view.py
+++ mpi_c/view.py
@@ -3,9 +3,13 @@ import numpy as np
 from matplotlib import cm
 
 dat = np.fromfile('test0.dat', dtype = 'uint8')
-dat = dat.reshape((10000,10000))
-print(dat.mean())
-#ig = Image.fromarray(dat.astype('uint8')).convert('P', palette = Image.WEB)
+dat = dat.reshape((25000,25000))
+#print(dat.mean())
+#x = dat[0:1000][0:1000]
+#print(type(x))
+#x = x.reshape((1000,1000))
+#.reshape((1000,1000))
+#ig = Image.fromarray(x.astype('uint8')).convert('P', palette = Image.WEB)
 #ig = Image.fromarray(np.uint8(cm.viridis(dat)*255))
 ig = Image.fromarray(np.uint8(cm.inferno(dat)*255))
 ig.save('test.png')
